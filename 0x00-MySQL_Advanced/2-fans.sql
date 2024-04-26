530255105c9d58512bd65f3f8a41e092ecfc6a41
O'Brien <abucheriobrien@gmail.com>
2024-04-26 04:14:15 +0300

    Best band ever! updatw0

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,8 @@
 -- Task 2: Rank country origins of bands by number of fans
 -- Import the metal_bands table dump
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin,
+    SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
