0f27387c24fa23678942fed16b2ee207b98c7a63
O'Brien <abucheriobrien@gmail.com>
2024-05-02 02:13:35 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Cache module implementing Redis caching.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -32,3 +32,52 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis using the given key and optionally applies
+        a conversion function.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Callable, optional): A callable function used to convert the
+                                     retrieved data. Defaults to None.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data, converted
+                                                 using the provided function
+                                                 if applicable.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieves data from Redis as a string.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Union[str, None]: The retrieved data as a string, or None if the
+                              key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieves data from Redis as an integer.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+
+        Returns:
+            Union[int, None]: The retrieved data as an integer, or None if the
+                              key does not exist.
+        """
+        return self.get(key, fn=int)
